476b409c54fb5649ad7f750f1486aef79176f133
DayoRodriguez <rodriguezperezdayan@gmail.com>
2025-10-10 11:59:23 -0400

    Implementadom el cargado de la info desde un .JSON

---
+++ GUI/main_window.py
@@ -0,0 +1,8 @@
+import flet as ft
+
+def main(page : ft.Page):
+    page.title = "Event Gestor"
+    page.window_width = 400
+    page.window_height = 300
+
+ft.app(target=main)
--- prueba.py
+++ prueba.py
@@ -2,6 +2,7 @@ from core.resource import Resource
 from core.events import Event
 from core.restriction import Restriction
 from core.events_planificator import Events_Planificator
+from core.events_data_saved_loader import Event_data_saved_loader
 
 r = Resource("Q1", "Quirofano 1", "material", False, {})
 # print(r)
@@ -11,6 +12,11 @@ m1 = Resource("M1", "Dr.Perez", "personal", "electro", False, {"specialty": "car
 e = Event("e1", "Operacion de Corazon", 1, "cardiologo", (2025,10,10),(2025,10,11), False,[m,m1], [r])
 # print(e)
 re = Restriction()
-print(re._there_is_only_specialist_in_charge(e))
+# print(re._there_is_only_specialist_in_charge(e))
 ep = Events_Planificator([])
-print(ep)
+# print(ep)
+
+personal = Event_data_saved_loader.load_nurses()
+# print(personal)
+n1 = Resource(personal[0]["id"], personal[0]["name"], personal[0]["type"], personal[0]["co_requested"], personal[0]["is_on_use"], personal[0]["attributes"])
+print(n1)
